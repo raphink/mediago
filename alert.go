0a4161db2e0dda42c2683c6043d4e771399a0d6d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-10 13:29:17 +0200

    Add URL to gist in alert email

--- alert.go
+++ alert.go
@@ -17,6 +17,9 @@ func SMTPAlert(cfg *config, a *account) (err error) {
 	msg := fmt.Sprintf("To: %s\r\n", strings.Join(cfg.Smtp.Recipients, ","))
 	msg += fmt.Sprintf("Subject: Mediathèque books for %s\r\n\r\n", a.Name)
 	msg += a.alerts(false, false)
+	if cfg.Gist.GistID {
+		msg += fmt.Sprintf("\nSee details on https://gist.github.com/%s\n", cfg.Gist.GistID)
+	}
 	err = smtp.SendMail(cfg.Smtp.Hostname+":"+strconv.Itoa(cfg.Smtp.Port),
 		auth,
 		cfg.Smtp.Username,
